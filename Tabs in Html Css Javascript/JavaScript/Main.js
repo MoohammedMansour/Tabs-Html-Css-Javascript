98cc99d1a50b8694fe364f8fb9d16ace8529c600
MoohammedMansour <97979389+MoohammedMansour@users.noreply.github.com>
2022-08-23 06:07:21 +0200

    Add files via upload

---
+++ Tabs in Html Css Javascript/JavaScript/Main.js
@@ -0,0 +1,14 @@
+const tabs = document.querySelectorAll('ul li');
+const divs = document.querySelectorAll('.content > div');
+tabs.forEach((tab) => {
+    tab.addEventListener("click", function(e) {
+        tabs.forEach((tab) => {
+            tab.classList.remove("active");
+        });
+        e.currentTarget.classList.add("active");
+        divs.forEach((div) => {
+            div.style.display = 'none';
+            document.querySelector('.' + e.currentTarget.dataset.content).style.display = 'flex';
+        })
+    })
+})
